ce75b7e48d67398ce366980be9010e589cc6e3a4
AnupGrahan <anup@parakhi.com>
2015-01-06 16:03:41 +0545

    final

---
+++ first.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "processor";
+$dbname = "todo";
+
+$task = $_POST['task'];
+$status = $_POST['status'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+	$sql = "INSERT INTO todo_list (task,status) VALUES ('".$task."','".$status."')";
+	if ($conn->query($sql) === TRUE) {
+	     echo $conn->insert_id;
+	} else {
+	    echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+}
+
+$conn->close();
+?> 
